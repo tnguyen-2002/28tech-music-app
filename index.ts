a9420eebf7ad073cac5d3d80b8c0ff20bc8c96bd
rei666 <h1.tnguyen088@gmail.com>
2024-11-13 23:42:38 +0700

    Init basic route

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-import express, {Express} from 'express';
+import express, {Express, Response, Request} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log("OK");
+const app: Express = express();
+const port: String = process.env.PORT;
+
+app.get("/topics", ( req: Request, res: Response ) => {
+    res.send("Album");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
